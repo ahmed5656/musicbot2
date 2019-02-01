6b4869e691acdf5bb35ae7f75e798009685cefa6
ahmed5656 <42041981+ahmed5656@users.noreply.github.com>
2019-02-01 05:40:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -425,7 +425,7 @@ function isYoutube(str) {
      خروج البوت من الروم
      prefix = ${prefix}
      ping = ${Date.now() - message.createdTimestamp}ms
-     By Nawaf   `)
+     By .AhmeeeD #0001   `)
 
       message.channel.send({embed});
 
